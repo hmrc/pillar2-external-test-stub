a6ede8ce740fa0c42f96faeede796f82852d55b4
olakunlekuye2 <olakunle.kuye2@digital.hmrc.gov.uk>
2024-11-19 12:40:03 +0000

    Formated appdependecies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,16 +4,15 @@ object AppDependencies {
 
   private val bootstrapVersion = "8.4.0"
 
-
   val compile: Seq[ModuleID] = Seq(
-    "org.typelevel"           %% "cats-core"                 % "2.12.0",
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion
+    "org.typelevel" %% "cats-core"                 % "2.12.0",
+    "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootstrapVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
-    "org.mockito"            % "mockito-core"            % "5.11.0",
-    "org.scalatestplus"      %% "mockito-4-11"            % "3.2.18.0"
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
+    "org.mockito"        % "mockito-core"           % "5.11.0",
+    "org.scalatestplus" %% "mockito-4-11"           % "3.2.18.0"
   ).map(_ % Test)
 
   val it: Seq[ModuleID] = Seq(
