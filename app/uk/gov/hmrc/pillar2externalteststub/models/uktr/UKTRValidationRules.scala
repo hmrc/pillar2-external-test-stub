00724f55dab02fae3f740e48b6ac0aa8fc9e055b
JamesMMiller <7511223+JamesMMiller@users.noreply.github.com>
2025-04-03 09:55:02 +0100

    Fixup

--- app/uk/gov/hmrc/pillar2externalteststub/models/uktr/UKTRValidationRules.scala
+++ app/uk/gov/hmrc/pillar2externalteststub/models/uktr/UKTRValidationRules.scala
@@ -29,7 +29,6 @@ object UKTRValidationRules {
   ): ValidationRule[T] =
     ValidationRule[T] { data =>
       if (data.obligationMTT && org.organisation.orgDetails.domesticOnly) {
-        println(s"obligationMTTRule invalid: data = $data, isDomestic = ${org.organisation.orgDetails.domesticOnly}")
         invalid(
           UKTRSubmissionError(
             InvalidReturn
@@ -46,7 +45,6 @@ object UKTRValidationRules {
       val isDomestic = org.organisation.orgDetails.domesticOnly
       (data.electionUKGAAP, isDomestic) match {
         case (true, false) =>
-          println(s"electionUKGAAPRule invalid: data = $data, isDomestic = $isDomestic")
           invalid(
             UKTRSubmissionError(
               InvalidReturn
