c01f8dfc56d3f58b9ff768be96794dbbf04e0ed7
Mohammad Dweik <180322631+MoDweik@users.noreply.github.com>
2025-05-16 15:20:01 +0100

    PIL-2044: O&S - Fix canAmend and dueDate (#80)
    _Replaces (https://github.com/hmrc/pillar2-external-test-stub/pull/77
    after https://github.com/hmrc/pillar2-external-test-stub/pull/78)_
    ### Changes
    - `dueDate` to equal the testOrg `registrationDate` + 18 months
    - `canAmend` to be `true` if the query is made within 12 months from the
    `dueDate`. The rejection of an amendment made after this date will be
    implemented in
    [PIL-1989](https://jira.tools.tax.service.gov.uk/browse/PIL-1989)

--- test/uk/gov/hmrc/pillar2externalteststub/controllers/ObligationsAndSubmissionsControllerSpec.scala
+++ test/uk/gov/hmrc/pillar2externalteststub/controllers/ObligationsAndSubmissionsControllerSpec.scala
@@ -20,9 +20,9 @@ import org.bson.types.ObjectId
 import org.mockito.ArgumentMatchers.{any, anyString}
 import org.mockito.Mockito.when
 import org.mockito.stubbing.OngoingStubbing
-import org.scalatest.OptionValues
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
+import org.scalatest.{Assertion, OptionValues}
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.inject.guice.GuiceApplicationBuilder
@@ -30,7 +30,7 @@ import play.api.libs.json.JsValue
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.api.{Application, inject}
-import uk.gov.hmrc.pillar2externalteststub.helpers.Pillar2Helper.ServerErrorPlrId
+import uk.gov.hmrc.pillar2externalteststub.helpers.Pillar2Helper.{AMENDMENT_WINDOW_MONTHS, FIRST_AP_DUE_DATE_FROM_REGISTRATION_MONTHS, ServerErrorPlrId}
 import uk.gov.hmrc.pillar2externalteststub.helpers.{ObligationsAndSubmissionsDataFixture, TestOrgDataFixture, UKTRDataFixture}
 import uk.gov.hmrc.pillar2externalteststub.models.error.ETMPError.{ETMPInternalServerError, NoDataFound, RequestCouldNotBeProcessed}
 import uk.gov.hmrc.pillar2externalteststub.models.error.OrganisationNotFound
@@ -134,6 +134,18 @@ class ObligationsAndSubmissionsControllerSpec
         secondObligationType mustBe GIR
       }
 
+      "should return the correct dueDate" in {
+        when(mockOrgService.getOrganisation(anyString())).thenReturn(Future.successful(nonDomesticOrganisation))
+        mockBySubmissionType(ORN_CREATE)
+
+        val result = route(app, createRequest()).value
+        status(result) mustBe OK
+
+        val dueDate = (contentAsJson(result) \\ "dueDate").head.as[LocalDate]
+
+        dueDate mustEqual nonDomesticOrganisation.organisation.orgDetails.registrationDate.plusMonths(18)
+      }
+
       "should handle submissions with country code for ORN submission type" in {
         when(mockOrgService.getOrganisation(anyString())).thenReturn(Future.successful(nonDomesticOrganisation))
         mockBySubmissionType(ORN_CREATE)
@@ -189,24 +201,29 @@ class ObligationsAndSubmissionsControllerSpec
         submissions.head.submissionType mustBe SubmissionType.GIR
       }
 
-      "should set canAmend to false when current date is after due date" in {
-        val pastDueOrg = domesticOrganisation.copy(
-          organisation = domesticOrganisation.organisation.copy(
-            accountingPeriod = domesticOrganisation.organisation.accountingPeriod.copy(
-              startDate = LocalDate.of(2022, 1, 1),
-              endDate = LocalDate.of(2022, 12, 31)
-            )
-          )
-        )
+      "set canAmend flag correctly based on due date" - {
+        val boundaryDate = LocalDate.now.minusMonths(FIRST_AP_DUE_DATE_FROM_REGISTRATION_MONTHS + AMENDMENT_WINDOW_MONTHS)
 
-        when(mockOrgService.getOrganisation(anyString())).thenReturn(Future.successful(pastDueOrg))
-        when(mockOasRepository.findByPillar2Id(anyString(), any[LocalDate], any[LocalDate])).thenReturn(Future.successful(Seq.empty))
+        def canAmendCheck(registrationDate: LocalDate, expectedStatus: Boolean): Assertion = {
+          val testOrg = configurableRegistrationDate.replace(registrationDate)(domesticOrganisation)
 
-        val result = route(app, createRequest(fromDate = "2022-01-01", toDate = "2023-01-01")).value
-        status(result) mustBe OK
+          when(mockOrgService.getOrganisation(anyString())).thenReturn(Future.successful(testOrg))
+          when(mockOasRepository.findByPillar2Id(anyString(), any[LocalDate], any[LocalDate]))
+            .thenReturn(Future.successful(Seq.empty))
 
-        val jsonResponse = contentAsJson(result)
-        (jsonResponse \ "success" \ "accountingPeriodDetails" \ 0 \ "obligations" \ 0 \ "canAmend").as[Boolean] mustBe false
+          val result = route(app, createRequest()).value
+          status(result) mustBe OK
+
+          (contentAsJson(result) \\ "canAmend").head.as[Boolean] mustBe expectedStatus
+        }
+
+        "false when current date is over 12 months after the dueDate" in {
+          canAmendCheck(boundaryDate.minusDays(1), expectedStatus = false)
+        }
+
+        "true when current date is within 12 months from the dueDate" in {
+          canAmendCheck(boundaryDate, expectedStatus = true)
+        }
       }
 
       "should return the correct response when no submissions exist" in {
