2167ceaf9a950e1e341f071b2243519d853838dd
JamesMMiller <7511223+JamesMMiller@users.noreply.github.com>
2025-07-31 10:49:01 +0100

    Bump boostrap and mongo versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.11.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.18.0"
+  private val hmrcMongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "org.typelevel"     %% "cats-core"                 % "2.13.0",
