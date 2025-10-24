7734feaa5c055f498fe75add77fad0cdef0c6db4
joshua-lamptey <220714391+joshua-lamptey@users.noreply.github.com>
2025-10-24 11:25:33 +0100

    [PIL-2182] - update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "org.typelevel"     %% "cats-core"                 % "2.13.0",
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
-    "com.beachape"      %% "enumeratum-play-json"      % "1.8.2",
+    "com.beachape"      %% "enumeratum-play-json"      % "1.9.0",
     "dev.optics"        %% "monocle-core"              % "3.3.0",
     "dev.optics"        %% "monocle-macro"             % "3.3.0"
   )
@@ -17,7 +17,7 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.mockito"        % "mockito-core"            % "5.15.2",
+    "org.mockito"        % "mockito-core"            % "5.20.0",
     "org.scalatestplus" %% "mockito-4-11"            % "3.2.18.0"
   ).map(_ % Test)
 
